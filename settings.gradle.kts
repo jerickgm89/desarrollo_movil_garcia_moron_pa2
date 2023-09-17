2d1040a5f3f18be7587597611c66a541f796ee8b
JErickDev <jerickgm89@gmail.com>
2023-09-17 00:04:16 -0500

    Comenzando el PA2

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Garcia_Moron_Jorge_PA2"
+include(":app")
